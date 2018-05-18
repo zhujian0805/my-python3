ca9a82900028c0ac24aafc927a8b72b1014e4cb1
James Zhu <zhujian0805@gmail.com>
2018-05-18 19:52:11 +0800

    This commit has the following change(s): ------------------------------------------------  M simu_write_files.py ------------------------------------------------

--- library/asyncio/simu_write_files.py
+++ library/asyncio/simu_write_files.py
@@ -20,10 +20,11 @@
 # ===============================================================================
 
 import asyncio
-from pprint import pprint
 import time
+import sys
 
-now = lambda : time.time()
+now = lambda: time.time()
+
 
 async def worker(filename):
     i = 0
@@ -34,16 +35,14 @@ async def worker(filename):
             if i > 10000:
                 break
 
-start = now()
-
-tasks = []
-
-for i in range(10):
-    fname = "file%s.txt" % str(i)
-    tasks.append(asyncio.ensure_future(worker(fname)))
-
-loop = asyncio.get_event_loop()
-
-loop.run_until_complete(asyncio.wait(tasks))
-
-print('TIME: ', now() - start)
+if __name__ == "__main__":
+    FILENAME = sys.argv[1]
+    count = int(sys.argv[2])
+    start = now()
+    tasks = []
+    for i in range(count):
+        fname = "%s%s.txt" % (FILENAME, str(i))
+        tasks.append(asyncio.ensure_future(worker(fname)))
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.wait(tasks))
+    print('TIME: ', now() - start)
